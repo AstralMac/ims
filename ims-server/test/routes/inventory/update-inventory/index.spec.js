197c9083eb626d6c151df372d33f9b69fb5b3bdc
Malcolm Abdullah <mabdullah4562@gmail.com>
2025-02-21 10:26:05 -0600

    Update

--- ims-server/test/routes/inventory/update-inventory/index.spec.js
+++ ims-server/test/routes/inventory/update-inventory/index.spec.js
@@ -4,65 +4,6 @@
  * File: index.spec.js
  * description: Tests for "Update Inventory Item" API
  */
-/*
-'use strict';
-const mongoose = require('mongoose');
-const request = require('supertest');
-const app = require('../../../../src/app');
-const {inventoryItem} = require('../../../../src/models/inventoryItem');
-const {updateInventoryItemSchema} = require('../../../../src/scripts/schemas');
-
-
-// Mock the Inventory Item schema Model
-jest.mock('../../../../src/models/inventoryItem');
-
-describe('PATCH /api/inventory/:id', ()=>{
-  it('should update the desired fields of an item', async ()=>{
-    inventoryItem.findByIdAndUpdate.mockResolvedValue();
-    const response = await request(app).patch('/api/inventory/650c1f1e1c9d440000a1b1c1').send({
-      price: 1000.00
-    });
-
-    //Debugging
-    console.log('response:',response.body);
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('Item was updated successfully');
-  });
-
-  it('should return errors for invalid input', async ()=>{
-    const response = await request(app).patch('/api/inventory/650c1f1e1c9d440000a1b1c1').send({
-      price: 'one-thousand dollars',
-      description: 12345 
-    });
-
-    // for debugging
-    console.log('response:',response.body);
-    expect(response.status).toBe(400);
-    expect(response.body.message).toBe('data/price should be number');
-  });
-  it('should return errors for item not found', async ()=>{
-    const response = await request(app).patch('/api/inventory/650c1f1e1c9d440000a145c2').send({
-      name: 'Faulty Item',
-      categoryId: 1000,
-      supplierId: 1,
-      description: 'Should fail',
-      quantity: 5,
-      price: 20.00
-    });
-
-    //debugging
-    console.log('response:',response.body);
-    expect(response.status).toBe(404);
-    expect(response.body.message).toBe('Item not found');
-  });
-});
-*/
-/**
- * Author: Malcolm Abdullah
- * Date: February 20th, 2025
- * File: index.spec.js
- * Description: Tests for "Update Inventory Item" API
- */
 'use strict';
 
 const mongoose = require('mongoose');
